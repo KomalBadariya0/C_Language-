29bf09e95d0f672a55149a3599ae5387b8d4bbd6
Komal Badariya <komalbadariya76@gmail.com>
2025-08-25 16:34:59 +0530

    Upload my folder in github

---
+++ Arithmetic Operator.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+	/*int a = 20;
+	int b = 30;
+	int c = a + b;
+	printf("%d",c);*/
+
+	/*int a = 20;
+	int b = 10;
+	int c = a - b;
+	printf("%d",c);*/
+
+	/*int a = 20;
+	int b = 7;
+	int c = a * b;
+	printf("%d",c);*/
+
+	/*int a = 20;
+	int b = 5;
+	int c = a / b;
+	printf("%d",c);*/
+
+	/*int a = 56;
+	int b = 34;
+	int c = 12;
+	int d = 2;
+	int e = 13;
+	int x = a*b+c/d-e;
+	printf("%d",x);*/
+
+	/*float a = 1.2;
+	float b = 6.7;
+	float c = 4.5;
+	int d = 2;
+	float x = a*b-c/d;
+	printf("%f",x);*/
+
+	return 0;
+}
